ff723c4d9add2353b208cb6f5f4e63e671c97e6b
Samantha L A Silva <samantha.athayde@americanas.io>
2023-08-10 16:03:52 -0300

    Add terraform directory

--- main.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-  project     = "terraform-pubsub"
-  region      = "us-central1"
-  zone        = "us-central1-c"
-}
---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.77.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "terraform-pubsub"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
